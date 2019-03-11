66ef65d9cf769abeb52ec4ffde97abd938e7f87e
Zagoshipda <john713@hanmail.net>
2019-03-11 18:32:09 +0900

    A-B

---
+++ boj/done/1001.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// ctrl alt n for run code ctrl alt m for stop the running code 
+
+int main(){
+    
+    int val1, val2;
+    std::cin >> val1 >> val2;
+    std::cout << val1 - val2;
+    
+    return 0;
+}
