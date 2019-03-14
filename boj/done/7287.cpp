d99319d6df0ad9b1258660bcf2d9433c91f3832e
Zagoshipda <john713@hanmail.net>
2019-03-14 19:50:39 +0900

    registration

---
+++ boj/done/7287.cpp
@@ -0,0 +1,10 @@
+// 등록 
+// 자신이 온라인 저지에서 맞은 문제의 개수와 아이디를 그대로 출력하는 프로그램을 작성하시오.
+// 첫 줄에 자신이 맞은 문제의 수, 둘째 줄에 아이디를 출력한다.
+
+#include <iostream>
+using namespace std;
+int main(){
+    cout << 25 << '\n';
+    cout << "dlwjsgud12" << '\n'; 
+}
