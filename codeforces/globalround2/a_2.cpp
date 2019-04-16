f8e421d421a0e61172825fc75bb98cd5c0e6dc21
Zagoshipda <john713@hanmail.net>
2019-04-17 02:26:05 +0900

    Ilya and a Colorful Walk

--- codeforces/globalround2/a_2.cpp
+++ codeforces/globalround2/a_2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-// #define int long long
+#define ll long long
 #define endl '\n'
 
 // int n, ans, a[10], s=1e18; 
