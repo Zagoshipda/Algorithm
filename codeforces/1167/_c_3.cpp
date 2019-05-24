a9c043aff7c071d2fd93d66659b40260ba809846
Zagoshipda <john713@hanmail.net>
2019-05-25 00:33:16 +0900

    News Distribution, by xiaowuc1

---
+++ codeforces/1167/_c_3.cpp
@@ -0,0 +1,70 @@
+// by, xiaowuc1
+
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+using namespace std;
+typedef long long ll;
+
+int par[1000000];
+int sz[1000000];
+
+int find(int x) {
+    return par[x] == x ? x : (par[x] = find(par[x]));
+}
+void merge(int x, int y) {
+    x = find(x);
+    y = find(y);
+    if(x == y) return;
+    sz[x] += sz[y];
+    par[y] = x;
+}
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++){
+        par[i] = i;
+        sz[i] = 1;
+    }
+    while(m--) {
+        int k;
+        cin >> k;
+        vector<int> v;      //vector 사용하기 
+        for(int i = 0; i < k; i++){
+            int t;
+            cin >> t;
+            v.push_back(t);
+        }
+        for(int i = 1; i < k; i++){     //i = 1부터 시작 
+            merge(v[i-1], v[i]);
+        } 
+    }
+    for(int i = 1; i <= n; i++){
+        cout << sz[find(i)] << " ";
+    }
+    cout << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    solve();
+    /*
+    int t;
+    cin >> t;
+    for(int i = 1; i <= t; i++) {
+        cout << "Case #" << i << ": ";
+        solve();
+    }
+    */
+}
