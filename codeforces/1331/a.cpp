1e5978d928e713a4208c2be21745c24ab3f52f7d
Zagoshipda <john713@hanmail.net>
2020-04-03 16:53:38 +0900

    cf 1331 April Fools Day Contest 2020 - A. Is it rated?

---
+++ codeforces/1331/a.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '\n'
+
+int main(){
+	IOS;
+
+    cout << "NO" << endl;
+
+    return 0;
+}
