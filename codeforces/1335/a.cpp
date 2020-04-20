2698a7a6ad4e11c0e2005264354f1d7cd7294443
Zagoshipda <john713@hanmail.net>
2020-04-21 00:30:23 +0900

    cf 1335 Round 634 div3 - A. Candies and Two Sisters

---
+++ codeforces/1335/a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '\n'
+#define ll long long    // range : -9*10^18 ~ 9*10^18
+
+int t;
+int n;
+
+int main(){
+    IOS;
+
+    cin >> t;
+    while(t--){
+        cin >> n;
+        cout << (n+1)/2-1 << endl;
+    }
+
+    
+
+    return 0;
+}
