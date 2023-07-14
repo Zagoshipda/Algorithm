92faa3e2309dc49de96f4a25dd3cc22a9b1bd807
Zagoshipda <john713@hanmail.net>
2023-07-14 11:16:32 +0900

    boj 9084 - Coin

---
+++ boj/done/9084.cpp
@@ -0,0 +1,58 @@
+/*
+    Coin - count the number of ways to make certain value k
+    https://www.acmicpc.net/problem/9084
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '\n'
+#define ll long long    // range : -9*10^18 ~ 9*10^18
+
+#define SIZE 20
+#define SIZE_PRICE 10'000
+#define SIZE_TARGET 10'000
+int T;
+int N;
+int coin[SIZE];
+int M;
+int dp[SIZE_TARGET + 1];    // number of ways to make value k
+
+void initialize(){
+    memset(dp, 0, sizeof dp);
+}
+
+void calculate_number_of_ways_to_make_target_price(){
+
+    dp[0] = 1;
+    for(int i=0; i<N; ++i){
+        for(int value=coin[i]; value<=M; ++value){
+            dp[value] += dp[value-coin[i]];
+        }
+    }
+
+    cout << dp[M] << endl;
+}
+
+int main(){
+    IOS;
+
+    cin >> T;
+
+    while(T--){
+        cin >> N;
+        for(int i=0; i<N; ++i){
+            cin >> coin[i];
+        }
+        cin >> M;
+
+        calculate_number_of_ways_to_make_target_price();
+
+        initialize();
+    }
+
+    return 0;
+}
