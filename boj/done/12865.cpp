f55fcaf32703b8f1f472e4ca0d8de67bd59c9db5
Zagoshipda <john713@hanmail.net>
2023-11-12 12:11:11 +0900

    boj 12865 - knapsack problem

--- boj/done/12865.cpp
+++ boj/done/12865.cpp
@@ -1,6 +1,11 @@
 /*
-    knapsack problem
     https://www.acmicpc.net/problem/12865
+    knapsack problem (all values integer)
+        NOTE : what happens when weights or values are real-numbers ?
+        time complexity O(N K) is pseudo-polynomial,
+        depends on the size of the input, and the numbers in the inputs
+        (here, the size of the knapsack)
+        therefore the time complexity may change
 
     optimizing space complexity
     https://www.acmicpc.net/source/22254620 (12 ms)
@@ -53,44 +58,52 @@ void knapsack_backtracking(int position, int current_value, int weight_left){
 
 int dp[SIZE + 1][MAX_WEIGHT + 1];
 
-// dp[M][N] max value at item 1...M, with N [weight left]
+// dp[M+1][N] max value at item 1...M, with [N weight left]
 void knapsack_DP_prefix_1(){
 
     for(int i=0; i<N; ++i){
         int weight = item[i].first;
         int value = item[i].second;
         // 1. NOT using i-th item
+        // NOTE : initialization starts from 0
         for(int weight_left=0; weight_left<=K; ++weight_left){
             dp[i+1][weight_left] = dp[i][weight_left];
         }
         // 2. using i-th item
         for(int weight_left=weight; weight_left<=K; ++weight_left){
-            dp[i+1][weight_left-weight] = max(dp[i+1][weight_left-weight], dp[i][weight_left] + value);
+            dp[i+1][weight_left - weight] = max(
+                dp[i+1][weight_left - weight],
+                dp[i][weight_left] + value
+            );
         }
     }
 
     // take maximum value among all possible states
     for(int weight_left=0; weight_left<=K; ++weight_left){
         // max value at item 0...N-1, with m weights left
+        // NOTE : index here at N
         max_value = max(max_value, dp[N][weight_left]);
     }
 
 }
 
-// dp[M][N] max value at item 1...M, with N [weight capacity]
+// dp[M][N] max value at item 1...M, with [N weight capacity]
 void knapsack_DP_prefix_2(){
-
-    for(int weight_left=item[0].first; weight_left<=K; ++weight_left){
-        dp[0][weight_left] = item[0].second;
+    // NOTE : initialization starts from item[0].weight to full capacity K
+    for(int capacity=item[0].first; capacity<=K; ++capacity){
+        dp[0][capacity] = item[0].second;
     }
 
     for(int i=1; i<N; ++i){
-        for(int weight_left=0; weight_left<=K; ++weight_left){
-            if(weight_left >= item[i].first){
-                dp[i][weight_left] = max(dp[i-1][weight_left-item[i].first] + item[i].second, dp[i-1][weight_left]);
+        for(int capacity=0; capacity<=K; ++capacity){
+            if(capacity >= item[i].first){
+                dp[i][capacity] = max(
+                    dp[i-1][capacity - item[i].first] + item[i].second,
+                    dp[i-1][capacity]
+                );
             }
             else{
-                dp[i][weight_left] = dp[i-1][weight_left];
+                dp[i][capacity] = dp[i-1][capacity];
             }
         }
     }
@@ -98,23 +111,30 @@ void knapsack_DP_prefix_2(){
     max_value = dp[N-1][K]; // max value at item 0...N-1, using K weight capacity
 }
 
-// NOTE : optimize space complexity
+// NOTE : optimize space complexity, dp on [possible weight value space]
 int dp_opt[MAX_WEIGHT + 1];   // dp[n] : max value at item n
 void knapsack_DP_prefix_3(){
     for(int i=0; i<N; ++i){
-        // for(int weight_left=0; weight_left<=K; ++weight_left){  // wrong answer
+        // // for(int weight_left=0; weight_left<=K; ++weight_left){  // wrong answer
         // for(int weight_left=K; weight_left>=0; --weight_left){  // (12 ms)
         //     if(weight_left >= item[i].first){
-        //         dp_opt[weight_left] = max(dp_opt[weight_left], dp_opt[weight_left-item[i].first] + item[i].second);
+        //         dp_opt[weight_left] = max(
+        //             dp_opt[weight_left],
+        //             dp_opt[weight_left - item[i].first] + item[i].second
+        //         );
         //     }
         // }
 
         // (8 ms)
-        for(int weight_left=K; weight_left>= item[i].first; --weight_left){
-            dp_opt[weight_left] = max(dp_opt[weight_left], dp_opt[weight_left - item[i].first] + item[i].second);
+        for(int capacity=K; capacity>=item[i].first; --capacity){
+            dp_opt[capacity] = max(
+                dp_opt[capacity],
+                dp_opt[capacity - item[i].first] + item[i].second
+            );
         }
     }
 
+    // original problem
     max_value = dp_opt[K];
 }
 
@@ -130,24 +150,116 @@ void knapsack_DP_prefix_3(){
 
 void knapsack_DP_suffix(){
 
-    for(int i=item[N-1].first; i<=K; ++i){
-        dp[N-1][i] = item[N-1].second;
+    for(int capacity=item[N-1].first; capacity<=K; ++capacity){
+        dp[N-1][capacity] = item[N-1].second;
     }
 
     for(int i=N-2; i>=0; --i){
-        for(int weight=0; weight<=K; ++weight){
+        for(int capacity=0; capacity<=K; ++capacity){
             // 1. can use i-th item, decide whether to use or not
-            if(weight - item[i].first >= 0){
-                dp[i][weight] = max(dp[i+1][weight - item[i].first] + item[i].second, dp[i+1][weight]);
+            if(capacity >= item[i].first){
+                dp[i][capacity] = max(
+                    dp[i+1][capacity - item[i].first] + item[i].second,
+                    dp[i+1][capacity]
+                );
             }
             // 2. can't using i-th item
             else{
-                dp[i][weight] = dp[i+1][weight];
+                dp[i][capacity] = dp[i+1][capacity];
             }
         }
     }
 
+    // original problem
     max_value = dp[0][K];
+}
+
+/*
+    WANT : maximum sum of values with fixed (integer) capacity
+    S ubproblem
+        if take i-th item,
+            value += item[i].value
+            capacity -= item[i].weight
+
+        each array dimension represents some information about the problem
+        (think n-dimensional array as n-dimensional matrix or cube)
+        dp[i][x] : sum of values [AFTER] handling (1...i)-th item with capacity x left
+
+    R ecurrence relation
+        choice : choose i-th item or not
+        prefix : we choose from 1...i-th item (left with i+1...n-th item)
+
+        2 possibilities : whether there is enough capacity for i-th item or not
+        i.e. we can take i-th item or not
+
+        when we can take i-th item
+        and if (weight_left + i-th weight) does not exceed the capacity K
+        if(weight_left+item[i].weight <= K){
+            dp[i][weight_left] = max(
+                dp[i-1][weight_left+item[i].weight] + item[i].value,
+                dp[i-1][weight_left]
+            )
+        }
+        when we can't take i-th item
+        else{
+            dp[i][weight_left] = dp[i-1][weight_left]
+        }
+
+    T opological order
+        think of DP as [local brute force]
+        for(int ith=1; ith<=N; ++ith){
+            for(int capacity=0...K){    // NOTE : capacity can start from 0
+                do recurrence relation above
+                    if(capacity+item[ith].weight <= K){
+                        dp[ith][capacity] = max(
+                            dp[ith-1][capacity+item[ith].weight] + item[ith].value,
+                            dp[ith-1][capacity]
+                        )
+                    }
+                    else{
+                        dp[ith][capacity] = dp[ith-1][capacity]
+                    }
+            }
+        }
+
+    B ase case
+        assume handlihng 0-th item with all capacities to be value of 0
+        (not taking any item yet)
+        for(int capacity=0...K){
+            dp[0][capacity] = 0;
+        }
+
+    O riginal problem
+        maximum sum of values
+        max(dp[N][i] for i=0...K)
+
+    T ime complexity
+*/
+
+void knapsack_review(){
+    // for(int weight_left=0; weight_left<=K-item[0].first; ++weight_left){
+    for(int weight_left=0; weight_left+item[0].first<=K; ++weight_left){
+        dp[0][weight_left] = item[0].second;
+    }
+
+    // NOTE : item index starts from 0
+    for(int ith=1; ith<N; ++ith){
+        for(int weight_left=0; weight_left<=K; ++weight_left){
+            if(weight_left + item[ith].first <= K){
+                dp[ith][weight_left] = max(
+                    dp[ith-1][weight_left + item[ith].first] + item[ith].second,
+                    dp[ith-1][weight_left]
+                );
+            }
+            else{
+                dp[ith][weight_left] = dp[ith-1][weight_left];
+            }
+        }
+    }
+
+    for(int weight_left=0; weight_left<=K; ++weight_left){
+        max_value = max(max_value, dp[N-1][weight_left]);
+    }
 }
 
 void solve(){
@@ -158,9 +270,11 @@ void solve(){
 
     // knapsack_DP_prefix_1();
     // knapsack_DP_prefix_2();
-    knapsack_DP_prefix_3();
+    // knapsack_DP_prefix_3();
 
     // knapsack_DP_suffix();
+
+    knapsack_review();
 
     cout << max_value << endl;
 }
@@ -171,17 +285,18 @@ int main(){
     cin >> N >> K;
     for(int i=0; i<N; ++i){
         cin >> W >> V;
-        // item[i].first = W;
-        // item[i].second = V;
+        item[i].first = W;
+        item[i].second = V;
 
-        for(int weight_left=K; weight_left>=W; --weight_left){
-            dp_opt[weight_left] = max(dp_opt[weight_left], dp_opt[weight_left - W] + V);
-        }
+        // space-optimized dp, direct to input
+        // for(int capacity=K; capacity>=W; --capacity){
+        //     dp_opt[capacity] = max(dp_opt[capacity], dp_opt[capacity - W] + V);
+        // }
     }
 
-    cout << dp_opt[K] << endl;
+    // cout << dp_opt[K] << endl;
 
-    // solve();
+    solve();
 
     return 0;
 }
