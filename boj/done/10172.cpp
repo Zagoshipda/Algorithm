f772dfbe40bc4dec580dd34c178ff223f3ce82f6
Zagoshipda <john713@hanmail.net>
2024-02-18 03:59:26 +0900

    boj 10172 - Dogs

---
+++ boj/done/10172.cpp
@@ -0,0 +1,41 @@
+/*
+    https://www.acmicpc.net/problem/10172
+    (Dogs)
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '\n'
+#define ll long long    // range : -9*10^18 ~ 9*10^18
+
+/*
+    use backslash to print out the required result
+*/
+
+void solve(){
+    // cout << "\\" << endl;
+    // cout << "\"\"\\" << endl;
+    cout << "|\\_/|" << endl;
+    cout << "|q p|   /}" << endl;
+    cout << "( 0 )\"\"\"\\" << endl;
+    cout << "|\"^\"`    |" << endl;
+    cout << "||_/=\\\\__|" << endl;
+}
+
+void input(){
+
+}
+
+int main(){
+    IOS;
+
+    input();
+
+    solve();
+
+    return 0;
+}
