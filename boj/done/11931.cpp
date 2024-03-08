fd8da18991bb9c25caa0e36196457edffabcdfad
Zagoshipda <john713@hanmail.net>
2024-03-08 23:41:01 +0900

    boj 11931 - sort 4

---
+++ boj/done/11931.cpp
@@ -0,0 +1,44 @@
+/*
+    https://www.acmicpc.net/problem/11931
+    (sort 4)
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '\n'
+#define ll long long    // range : -9*10^18 ~ 9*10^18
+
+/*
+
+*/
+
+const int NUM = 1'000'000;
+int N;
+int arr[NUM + 1];
+
+void solve(){
+    sort(arr+1, arr+1+N, greater<int>());
+    for(int i=1; i<=N; ++i){
+        cout << arr[i] << endl;
+    }
+}
+
+void input(){
+    cin >> N;
+    for(int i=1; i<=N; ++i){
+        cin >> arr[i];
+    }
+}
+
+int main(){
+    IOS;
+
+    input();
+
+    solve();
+
+    return 0;
+}
