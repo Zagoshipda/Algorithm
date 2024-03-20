a6d38809691ba0a403b090aef18b6291f77acb61
Zagoshipda <john713@hanmail.net>
2024-03-20 14:15:00 +0900

    boj 10998 - A x B

---
+++ boj/done/10998.cpp
@@ -0,0 +1,36 @@
+/*
+    https://www.acmicpc.net/problem/10998
+    (A X B)
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '\n'
+#define ll long long    // range : -9*10^18 ~ 9*10^18
+
+/*
+
+*/
+
+int A, B;
+
+void solve(){
+    cout << A * B << endl;
+}
+
+void input(){
+    cin >> A >> B;
+}
+
+int main(){
+    IOS;
+
+    input();
+
+    solve();
+
+    return 0;
+}
