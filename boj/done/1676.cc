64a8aa7b54558d2f37c08a5eebfc0b9c6c5cd0d3
Zagoshipda <john713@hanmail.net>
2024-03-30 05:13:52 +0900

    boj 1676 - trailing zeros on factorial

---
+++ boj/done/1676.cc
@@ -0,0 +1,65 @@
+/*
+    https://www.acmicpc.net/problem/1676
+    (trailing zeros on factorial)
+
+    similar problem
+        https://www.acmicpc.net/problem/2004
+        (trailing zeros on combination)
+
+        https://www.acmicpc.net/problem/1564
+        (factorial 5)
+
+    solutions
+        https://www.acmicpc.net/source/226577
+        (koosaga, 0 ms) : implemented in solve_2()
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '\n'
+#define ll long long    // range : -9*10^18 ~ 9*10^18
+
+/*
+    10! = 10 9 8 7 6 5 4 3 2 1
+          5*2        5
+*/
+
+int N;
+
+void solve_1(){
+    int cnt = 0;
+    for(int factor=5; factor<=N; factor*=5){
+        // for(int num=factor; num<=N; num+=factor){
+        //     ++cnt;
+        // }
+        cnt += N/factor;
+    }
+    cout << cnt << endl;
+}
+
+void solve_2(){
+    cout << (N / 5 + N / 25 + N / 125) << endl;
+}
+
+void solve(){
+    // solve_1();
+
+    solve_2();
+}
+
+void input(){
+    cin >> N;
+}
+
+int main(){
+    IOS;
+
+    input();
+
+    solve();
+
+    return 0;
+}
