cb62f5775d002751b57c35ba999e04ea480c825c
Zagoshipda <john713@hanmail.net>
2024-04-18 12:32:50 +0900

    boj 1237 - solution

---
+++ boj/done/1237.cc
@@ -0,0 +1,34 @@
+/*
+    https://www.acmicpc.net/problem/1237
+    (solution)
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '\n'
+#define ll long long    // range : -9*10^18 ~ 9*10^18
+
+/*
+
+*/
+
+void solve(){
+    cout << "문제의 정답" << endl;
+}
+
+void input(){
+
+}
+
+int main(){
+    IOS;
+
+    input();
+
+    solve();
+
+    return 0;
+}
