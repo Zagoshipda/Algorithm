4e1f5451de3330e13eee77820ce24a2b561e1f41
Zagoshipda <john713@hanmail.net>
2024-04-23 23:55:37 +0900

    boj 2743 - word length

---
+++ boj/done/2743.cc
@@ -0,0 +1,36 @@
+/*
+    https://www.acmicpc.net/problem/2743
+    (word length)
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '\n'
+#define ll long long    // range : -9*10^18 ~ 9*10^18
+
+/*
+
+*/
+
+string word;
+
+void solve(){
+    cout << word.size() << endl;
+}
+
+void input(){
+    cin >> word;
+}
+
+int main(){
+    IOS;
+
+    input();
+
+    solve();
+
+    return 0;
+}
