4ad4ee92292d17010e12d92a66f75d127bc30d05
Zagoshipda <john713@hanmail.net>
2024-04-28 19:13:18 +0900

    boj 11382 - little jeongmin

---
+++ boj/done/11382.cc
@@ -0,0 +1,36 @@
+/*
+    https://www.acmicpc.net/problem/11382
+    (little jeongmin)
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '\n'
+#define ll long long    // range : -9*10^18 ~ 9*10^18
+
+/*
+
+*/
+
+ll A, B, C;
+
+void solve(){
+    cout << A + B + C << endl;
+}
+
+void input(){
+    cin >> A >> B >> C;
+}
+
+int main(){
+    IOS;
+
+    input();
+
+    solve();
+
+    return 0;
+}
