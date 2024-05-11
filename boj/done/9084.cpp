5344363ff0379acf00b9f35bd476de0323f536f9
Zagoshipda <john713@hanmail.net>
2024-05-11 15:09:09 +0900

    boj 9084 - coin

--- boj/done/9084.cpp
+++ boj/done/9084.cpp
@@ -1,7 +1,12 @@
 /*
-    Coin - count the number of ways to make certain value k
     https://www.acmicpc.net/problem/9084
+    (Coin) : count the number of ways to make certain value k
 
+    similar problem
+        https://www.acmicpc.net/problem/3067
+        (Coins) : same problem
+        https://www.acmicpc.net/problem/2293
+        (coin 1)
 */
 
 #include <bits/stdc++.h>
@@ -11,6 +16,16 @@ using namespace std;
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define endl '\n'
 #define ll long long    // range : -9*10^18 ~ 9*10^18
+
+/*
+    2
+    1 2
+    2
+    // 1+1 or 2
+    0 1 2 dp
+    1 0 0
+
+*/
 
 #define SIZE 20
 #define SIZE_PRICE 10'000
@@ -26,9 +41,9 @@ void initialize(){
 }
 
 void calculate_number_of_ways_to_make_target_price(){
-
     dp[0] = 1;
     for(int i=0; i<N; ++i){
+        // NOTE : accumulating result from the past, in ascending value order
         for(int value=coin[i]; value<=M; ++value){
             dp[value] += dp[value-coin[i]];
         }
@@ -37,9 +52,7 @@ void calculate_number_of_ways_to_make_target_price(){
     cout << dp[M] << endl;
 }
 
-int main(){
-    IOS;
-
+void input(){
     cin >> T;
 
     while(T--){
@@ -53,6 +66,12 @@ int main(){
 
         initialize();
     }
+}
+
+int main(){
+    IOS;
+
+    input();
 
     return 0;
 }
