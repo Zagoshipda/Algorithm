e600d43a90d661e13d28ef58a7f685c62d36f968
Zagoshipda <john713@hanmail.net>
2024-08-16 13:12:56 +0900

    boj 10951 - A+B - 4

--- boj/done/10951.cpp
+++ boj/done/10951.cpp
@@ -1,12 +1,27 @@
-//A+B (4) 입력이 있는 한 계속해서 출력하기 
-// -> while 조건에 입력을 넣어버리는 것으로 해결할 수 있다 ....!! 
+/*
+    https://www.acmicpc.net/problem/10951
+        A+B (4) 입력이 있는 한 계속해서 출력하기
+        while 조건에 입력을 넣어버리는 것으로 해결
+*/
 
 #include <iostream>
 using namespace std;
 
-int main(){
-    int a, b;
-    while(cin >> a >> b){   
+int a, b;
+int solve_wrong(){
+    // output exceeded
+    while(true){
+        cin >> a >> b;
         cout << a + b << "\n";
     }
+}
+
+int solve(){
+    while(cin >> a >> b){
+        cout << a + b << "\n";
+    }
+}
+
+int main(){
+    solve();
 }
